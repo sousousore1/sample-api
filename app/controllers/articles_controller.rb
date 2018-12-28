19e3abdb1498e858630a445464429aeba5ea50ab
Soichi Saito <soichi.saito@bizreach.co.jp>
2018-12-28 16:13:19 +0900

    Add articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
     articles = Sample::Core::Article.all
     render json: articles
   end
+
+  def show
+    article = Sample::Core::Article.find(params[:id])
+    render json: article
+  end
 end
